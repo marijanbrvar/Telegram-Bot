9e03cb7fc0dd91590c947f9e3e14a01e2cb738d4
Marijan Brvar <marijan.brvar@gmail.com>
2021-04-02 14:27:28 +0200

    Fix offenses

--- spec/bot_test.rb
+++ spec/bot_test.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/motivation'
 describe Motivation do
-  let(:quote_of_the_day){Motivation.new}
+  let(:quote_of_the_day) { Motivation.new }
 
   describe '#initialize' do
-    let(:value) {quote_of_the_day.value}
+    let(:value) { quote_of_the_day.value }
 
     it 'returns true when object passwed to array' do
       expect(value.is_a?(Array)).to be(true)
