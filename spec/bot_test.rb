e805bb2bcf487eeebffd6f84a93d0bd8ce208ac5
Marijan Brvar <marijan.brvar@gmail.com>
2021-04-02 15:26:12 +0200

    Add jokes and motivation tests

--- spec/bot_test.rb
+++ spec/bot_test.rb
@@ -1,12 +1,45 @@
 require_relative '../lib/motivation'
+require_relative '../lib/jokes'
+
 describe Motivation do
-  let(:quote_of_the_day) { Motivation.new }
+  let(:quote) { Motivation.new }
 
-  describe '#initialize' do
-    let(:value) { quote_of_the_day.value }
+  describe '#select_random' do
+    context 'selecting randomly' do
+      it 'returns a key and a value' do
+        expect(quote.select_random.length).to eql(2)
+      end
+    end
 
-    it 'returns true when object passwed to array' do
-      expect(value.is_a?(Array)).to be(true)
+    it 'Returns false for a hash width key/value pairs' do
+      expect(quote.select_random.length).to_not eql(1)
+    end
+  end
+
+  describe '#make_an_request' do
+    let(:request) { Motivation.new }
+
+    it 'returns a JSON' do
+      expect(request.make_an_request.class).to be(Array)
+    end
+
+    it 'Must not return empty array' do
+      expect(request.make_an_request.length).to_not eql(0)
+    end
+  end
+end
+
+describe Jokes do
+  describe '#make_an_request' do
+    let(:joke) { Jokes.new }
+    let(:request_joke) { joke.make_an_request }
+
+    it 'should not return an empty array' do
+      expect(request_joke.length).to_not eql(0)
+    end
+
+    it 'should return a hash if the request is right' do
+      expect(request_joke.class).to eql(Hash)
     end
   end
 end
