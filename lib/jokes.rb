9d61e5acac4f3c8454aaaa6c10edba0d2185953f
Marijan Brvar <marijan.brvar@gmail.com>
2021-04-04 10:04:56 +0200

    Replice endpoint with more noraml jokes :-)

--- lib/jokes.rb
+++ lib/jokes.rb
@@ -10,7 +10,7 @@ class Jokes
   end
 
   def make_an_request
-    url = 'https://api.yomomma.info'
+    url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=sexist&type=single'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     JSON.parse(response)
